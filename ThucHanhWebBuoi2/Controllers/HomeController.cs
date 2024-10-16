5eba097260e91452ca833a6c0053f162b491f245
Acer <Acer@LAPTOP-6D91JS7D>
2024-10-17 02:21:03 +0700

    Add project files.

---
+++ ThucHanhWebBuoi2/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThucHanhWebBuoi2.Controllers
+{
+    public class HomeController : Controller
+    {
+   
+        public ActionResult test()
+        {
+            ViewBag.Message = "Trang Chủ";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
